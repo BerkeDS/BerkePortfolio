d1b339494d70df6929e801ef8eefae76754c8187
Berke <berke.d.sefik@gmail.com>
2023-07-13 00:50:50 +0300

    CV,Github link ve yeni sertifikalar eklendi

--- components/Home/About.js
+++ components/Home/About.js
@@ -12,7 +12,16 @@ function About(){
                 <h1 style={{fontSize:"3.6rem"}}>Hi I&apos;m Berke</h1>
                 <h3 style={{color: "brown",fontStyle: "italic"}}>a Developer</h3>
                 <p>
-                My programming journey began with c# and desktop applications. after c# I learned asp .net, sql server and ado net and followed by entity framework core .net core. Actually i towords .net core beacause i wanted to develop cross-platfom apps. I did a litte research and i  changed my mind to learn Javascript and with js i learned react, redux, NoSql Database(mongodb) and mongoose. Now i&apos;m working with those but ofcourse technology always progressing and we need the keep up with it.
+                My programming journey began with c# and desktop applications. after c# I learned asp .net, sql server and ado net and followed by entity framework core .net core. Actually i towords .net core beacause i wanted to develop cross-platfom apps. I did a litte research and i  changed my mind to learn Javascript and with js i learned react and redux. Now i&apos;m working with python and data analysis but of course technology always progressing and we need the keep up with it.
+                </p>
+                <p>
+                    <span> This site is developed with Next.js, React and redux </span>
+                    <span></span>
+
+                    <a href="https://github.com/BerkeDS" target="_blank" rel="noreferrer">Github Profile</a>
+                    <a href="https://www.freecodecamp.org/BerkeDS" target="_blank" rel="noreferrer">FreeCodeCamp Profile</a>
+                    <a href="https://www.hackerrank.com/berke_d_sefik?h_r=internal-search&hr_r=1" target="_blank" rel="noreferrer">Hackerrank Profile</a>
+                    <a href="/assets/documents/Universite.pdf" target="_blank" rel="noreferrer">CV</a>
                 </p>
             </div>
         </section>
--- store/features/certificateCardSlice.js
+++ store/features/certificateCardSlice.js
@@ -8,7 +8,7 @@ const initialState = {
             imgSRC: '/assets/images/JSfreeCodeCamp.png',
             title: "freeCodeCamp",
             certificateName: "JS Certificate",
-            titleLink: "https://www.freecodecamp.org/",
+            titleLink: "https://freecodecamp.org/certification/BerkeDS/javascript-algorithms-and-data-structures",
             certificateLink: "https://freecodecamp.org/certification/BerkeDS/javascript-algorithms-and-data-structures",
             description: "JavaScript Algorithms and Data Structures Cerficitace from freeCodeCamp"
         },
@@ -17,7 +17,7 @@ const initialState = {
             imgSRC: '/assets/images/responsiveWebDesign.png',
             title: "freeCodeCamp",
             certificateName: "Web Design",
-            titleLink: "https://www.freecodecamp.org/",
+            titleLink: "https://freecodecamp.org/certification/BerkeDS/responsive-web-design",
             certificateLink: "https://freecodecamp.org/certification/BerkeDS/responsive-web-design",
             description: "Responsive Web Design Cerficitace from freeCodeCamp"
         },
@@ -26,7 +26,7 @@ const initialState = {
             imgSRC: '/assets/images/certificateCss.png',
             title: "HackerRank",
             certificateName: "CSS Certificate",
-            titleLink: "https://www.hackerrank.com/",
+            titleLink: "https://www.hackerrank.com/certificates/c9350c563f2d",
             certificateLink: "https://www.hackerrank.com/certificates/c9350c563f2d",
             description: "CSS Cerficitace from HackerRank"
         },
@@ -35,9 +35,27 @@ const initialState = {
             imgSRC: '/assets/images/JSBasicCertificate.png',
             title: "HackerRank",
             certificateName: "JS(Basic) Certificate",
-            titleLink: "https://www.hackerrank.com/",
+            titleLink: "https://www.hackerrank.com/certificates/b9ffd025195b",
             certificateLink: "https://www.hackerrank.com/certificates/b9ffd025195b",
             description: "JavaScript(Basic) Cerficitace from HackerRank"
+        },
+        {
+            id: "4",
+            imgSRC: '/assets/images/dataCer.png',
+            title: "freeCodeCamp",
+            certificateName: "Data Analysis with Python",
+            titleLink: "https://www.freecodecamp.org/certification/BerkeDS/data-analysis-with-python-v7",
+            certificateLink: "https://www.freecodecamp.org/certification/BerkeDS/data-analysis-with-python-v7",
+            description: "Data Analysis Cerficitace from freeCodeCamp"
+        },
+        {
+            id: "5",
+            imgSRC: '/assets/images/pythonCer.png',
+            title: "freeCodeCamp",
+            certificateName: "Scientific Computing with Python",
+            titleLink: "https://www.freecodecamp.org/certification/BerkeDS/scientific-computing-with-python-v7",
+            certificateLink: "https://www.freecodecamp.org/certification/BerkeDS/scientific-computing-with-python-v7",
+            description: "Python Cerficitace from freeCodeCamp"
         },
     ]
 };
